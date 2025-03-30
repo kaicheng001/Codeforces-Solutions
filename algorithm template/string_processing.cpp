27363ef9d51ac4f294e8aa063cf831ceb86c9134
kaicheng <125368441+kaicheng001@users.noreply.github.com>
2025-03-30 17:16:10 +0800

    add is_panlindrome function

--- algorithm template/string_processing.cpp
+++ algorithm template/string_processing.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+// 回文串判断
+bool is_palindrome(const string &s) {
+  int n = s.size();
+  for (int i = 0; i < n / 2; i++) {
+    if (s[i] != s[n - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 // KMP算法 (字符串匹配)
 vector<int> kmp(const string &s) {
   int n = s.size();
