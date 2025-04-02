51a1c0ee12c4a48da14aa32d02eafd5447963fb4
kaicheng <125368441+kaicheng001@users.noreply.github.com>
2025-04-02 23:32:27 +0800

    optimize the cin of CF templates

--- template/codeforces_template_samples.cpp
+++ template/codeforces_template_samples.cpp
@@ -5,14 +5,30 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 
-void solve() {}
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define mod 1000000007 // 10^9 + 7
+#define MOD 998244353 // 998244353
+
+inline ll read() {
+  ll s = 0, w = 1;
+  char ch = getchar();
+  while (ch < '0' || ch > '9') {
+    if (ch == '-')
+      w = -1;
+    ch = getchar();
+  }
+  while (ch >= '0' && ch <= '9')
+    s = (s << 3) + (s << 1) + (ch ^ 48), ch = getchar();
+  return s * w;
+}
+
+void solve() {
+  
+}
 
 signed main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
-  int t;
-  cin >> t;
+  fastio;
+  ll t = read();
   while (t--) {
     solve();
   }
