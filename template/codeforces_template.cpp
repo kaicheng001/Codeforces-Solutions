4212f5e613e242ba5d1819263de4fe7e7d08a49a
kaicheng <125368441+kaicheng001@users.noreply.github.com>
2025-04-06 16:49:04 +0800

    update Codeforces template

---
+++ template/codeforces_template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+#define mod 1000000007 // 10^9 + 7
+#define MOD 998244353  // 998244353
+
+void solve() {
+  
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int tt;
+  cin >> tt;
+  // tt = 1;
+  while (tt--) {
+    solve();
+  }
+  return 0;
+}
